a5a8f840212eaee9acb6809efe0c9b4eb2ba0691
EMMANUEL RUIZ GARIBAY <emmanuel.ruiz0221@alumnos.udg.mx>
2022-11-25 17:47:24 -0600

    Evitar Colisión en las Horas de Atención

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -7,6 +7,7 @@ use App\Models\Appointment;
 use App\Models\Speciality;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AppointmentController extends Controller
 {
@@ -32,7 +33,7 @@ class AppointmentController extends Controller
         return view('appointments.create', compact('specialties', 'doctors', 'intervals'));
     }
 
-    public function store(Request $request){
+    public function store(Request $request, HorarioServiceInterface $horarioServiceInterface){
 
         $rules = [
             'scheduled_time' => 'required',
@@ -48,7 +49,31 @@ class AppointmentController extends Controller
             'type.required' => 'Debe seleccionar el tipo de consulta.',
             'description.required' => 'Debe poner sus sintomas.'
         ];
-        $this->validate($request, $rules, $messages);
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        $validator->after(function ($validator) use ($request, $horarioServiceInterface) {
+            
+            $date = $request->input('scheduled_date');
+            $doctorId = $request-> input('doctor_id');
+            $scheduled_time = $request->input('scheduled_time');
+            if($date && $doctorId && $scheduled_time){
+                $start = new Carbon($scheduled_time);
+            }else{
+                return;
+            }
+
+            if (!$horarioServiceInterface->isAvailableInterval($date, $doctorId, $start)) {
+                $validator->errors()->add(
+                    'available_time', 'La hora seleccionada ya se encuentra reservada por otro paciente.'
+                );
+            }
+        });
+
+        if ($validator->fails()) {
+            return back()
+                        ->withErrors($validator)
+                        ->withInput();
+        }
 
         $data = $request->only([
             'scheduled_date',
--- app/Services/HorarioService.php
+++ app/Services/HorarioService.php
@@ -14,6 +14,14 @@ class HorarioService implements HorarioServiceInterface{
         return $day;
     }
 
+    public function isAvailableInterval($date, $doctorId, Carbon $start){
+        $exists = Appointment::where('doctor_id', $doctorId)
+                ->where('scheduled_date', $date)
+                ->where('scheduled_time', $start->format('H:i:s'))
+                ->exists();
+            return !$exists;
+    }
+
     public function getAvailableIntervals($date, $doctorId){
         $horario = Horarios::where('active', true)
             ->where('day', $this->getDayFromDate($date))
@@ -47,15 +55,12 @@ class HorarioService implements HorarioServiceInterface{
             $intervalo = [];
             $intervalo['start'] = $start->format('g:i A');
 
-            $exists = Appointment::where('doctor_id', $doctorId)
-            ->where('scheduled_date', $date)
-            ->where('scheduled_time', $start->format('H:i:s'))
-            ->exists();
+            $available = $this->isAvailableInterval($date, $doctorId, $start);
 
             $start->addMinutes(30);
             $intervalo['end'] = $start->format('g:i A');
 
-            if(!$exists){
+            if($available){
                 $intervalos []= $intervalo;
             }
             
